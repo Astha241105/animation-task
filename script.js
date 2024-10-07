38b24082bbe779f28a2fa2c63e7d61ca52728a6e
Astha241105 <astha241105@gmail.com>
2024-10-07 22:12:59 +0530

    started making animation

---
+++ script.js
@@ -0,0 +1,11 @@
+function getRandom(){
+   return Math.floor(Math.random()*8)}
+chooseBlock=getRandom();
+function main(ctime){
+    windows.requestAnimationFrame(main);
+    
+}
+function gamelogic(){
+    block=document.createElement('div');
+}
+windows.requestAnimationFrame(main);
