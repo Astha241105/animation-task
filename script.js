1df1d6edfc6de0d38cf26ce8f99872d279da6f82
Astha241105 <astha241105@gmail.com>
2024-10-08 13:08:00 +0530

    added animation

--- script.js
+++ script.js
@@ -2,17 +2,31 @@ let speed=2;
 function getRandom(){
    return Math.floor(Math.random()*8)}
 chooseBlock=getRandom();
-const canvas=getElementById('canvas');
-let ctx=canvas.getContext('2d');
-const canvas.width=600;
-const images=['cat/Jump1.png','cat/Jump2.png','cat/Jump3.png','cat/Jump4.png','cat/Jump5.png','cat/Jump6.png','cat/Jump7.png','cat/Jump8.png']
+const canvas1=document.getElementById('canvas');
+const ctx=canvas1.getContext('2d');
+const images=['cat/Jump1.png','cat/Jump2.png','cat/Jump3.png','cat/Jump4.png','cat/Jump5.png','cat/Jump6.png','cat/Jump7.png','cat/Jump8.png'];
+const imageloaded=[];
 let curFrame=0;
-function main(){
-    ctx.clearRect(0,0,600,600);
-    ctx.fillRect(50,50,50,50);
-    ctx.drawImage(images[curFrame],0,0,canvas.height,canvas.width);
-    curFrame=curFrame+1;
-    requestAnimationFrame(main);
-
+function preloading(images,callback){
+    let loadcount=0;
+    images.forEach((loc,i) =>{
+        const img=new Image();
+        img.src=loc;
+        img.onload=function(){
+            imageloaded[i]=img;
+            loadcount++;
+            if(loadcount===images.length){
+                callback();
+            }
+        }
+    });
 }
-windows.requestAnimationFrame(main);
+function newframe(){
+    ctx.clearRect(0,0,canvas1.width,canvas1.height);
+    ctx.drawImage(imageloaded[curFrame],0,0,canvas1.height,canvas1.width);
+    curFrame=(curFrame+1)%images.length;
+    requestAnimationFrame(newframe);
+}
+preloading(images,function(){
+    requestAnimationFrame(newframe);   
+});
