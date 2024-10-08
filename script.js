49b00db91129ea477d75377fc9841fea9b917659
Astha241105 <astha241105@gmail.com>
2024-10-08 19:54:54 +0530

    added multiple canvas

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let speed=2;
 function getRandom(){
-   return Math.floor(Math.random()*8)}
-chooseBlock=getRandom();
-const canvas1=document.getElementById('canvas');
-const ctx=canvas1.getContext('2d');
+   return Math.floor(Math.random()*9);}
+let canvas=document.getElementById(`canvas${getRandom()}`);
+let ctx=canvas.getContext('2d');
 const images=['cat/Jump1.png','cat/Jump2.png','cat/Jump3.png','cat/Jump4.png','cat/Jump5.png','cat/Jump6.png','cat/Jump7.png','cat/Jump8.png'];
 const imageloaded=[];
+let lasttime=0;
+const frameduration=300;
 let curFrame=0;
 function preloading(images,callback){
     let loadcount=0;
@@ -21,11 +21,16 @@ function preloading(images,callback){
         }
     });
 }
-function newframe(){
-    ctx.clearRect(0,0,canvas1.width,canvas1.height);
-    ctx.drawImage(imageloaded[curFrame],0,0,canvas1.width,canvas1.height);
+function newframe(timetaken){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(imageloaded[curFrame],0,0,canvas.width,canvas.height);
+
+    const elapsedtime=timetaken-lasttime;
+
+    if(elapsedtime>frameduration){
     curFrame=(curFrame+1)%images.length;
-    requestAnimationFrame(newframe);
+      lasttime=timetaken;}
+      requestAnimationFrame(newframe);
 }
 preloading(images,function(){
     requestAnimationFrame(newframe);   
